09e3b6d00cca6332a622c0ea2ae83480f313998f
miyamoto <atsushi.am.miyamoto@nts.ricoh.co.jp>
2015-06-19 17:54:43 +0900

    add logic

--- 01/index.js
+++ 01/index.js
@@ -1,30 +1,30 @@
 'use strict';
 
 window.onload = function() {
-  var username = document.getElementById('username');
-  var password = document.getElementById('password');
+  var username = document.getElementsByName('username')[0];
+  var password = document.getElementsByName('password')[0];
   var login = document.getElementById('btn_login');
   var message = document.getElementById('message');
-  
-  username.addEventListener('input', function() {
-    // alert('user:'+username.value);
-    message.value = username.value;
 
-  });
-  password.addEventListener('input', function() {
-    alert('password:'+password.value);
+  login.addEventListener('click', function(e) {
+    try {
+      if(username.value.length < 2 || username.value.length > 8)
+        throw 'usernameは2～8文字です';
 
-  });
+      if(username.value.match(/[^(a-z|\-)]/))
+        throw 'usernameに使える文字は英小文字と-(ハイフン)です';
 
-  login.addEventListener('click', function(){
-    // validate username and password
-    if(username.value.match('/\l/')){
-      message.value = 'true';
-    }else{
-      message.value = 'false';
+      if(password.value.length < 6 || password.value.length > 24)
+        throw 'passwordは6～24文字です';
+
+      if(password.value.match(/[^(a-zA-z|\-|\+|\!|\@)]/))
+        throw 'passwordに使える文字は英小大文字と-,+,!,@です';
+
+　　　message.value = '';
+
+    } catch(err) {
+      message.value = err;
+      e.preventDefault();
     }
-
-    alert('user:'+username.value);
-    alert('PAS:'+password.value);
   });
 };
