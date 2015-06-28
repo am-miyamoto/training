b8c53ad13cb222990f4b1b9b9f95162f69f18130
am-miyamoto <atsushi.am.miyamoto@nts.ricoh.co.jp>
2015-06-29 00:33:05 +0900

    add test of invalid username

--- 04/test/validation.js
+++ 04/test/validation.js
@@ -16,6 +16,17 @@ var username_true = [
   , 'aB-_cD'
 ];
 
+var username_false = [
+    'aaa'
+  , 'abc'
+  , 'ABC'
+  , 'aBc'
+  , '!_-_'
+  , '-+!@#*'
+  , 'abcdefghi'
+  , 'ABCDEFGHI'
+];
+
 var password_true = [
     'aaaaaa'
   , 'abcdef'
@@ -32,7 +43,7 @@ var password_true = [
 ];
 
 describe('validation username', function() {
-  it('username is true', function(done) {
+  it('username is valid', function(done) {
     username_true.forEach(function(username) {
       console.log(username);
       var errors = validater.validation({ username: username, password: 'aaaaaa' });
@@ -40,10 +51,18 @@ describe('validation username', function() {
     });
     done();
   });
+  it('username is invalid', function(done) {
+    username_false.forEach(function(username) {
+      console.log(username);
+      var errors = validater.validation({ username: username, password: 'aaaaaa' });
+      assert.notStrictEqual(errors.length, 0);
+    });
+    done();
+  });
 });
 
 describe('validation password', function() {
-  it('password is true', function(done) {
+  it('password is valid', function(done) {
     password_true.forEach(function(password) {
       console.log(password);
       var errors = validater.validation({ username: 'aaaaaaa', password: password });
