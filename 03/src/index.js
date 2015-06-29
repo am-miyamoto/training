dc561b6d58fb460b25282bd8deef590584d123f2
am-miyamoto <atsushi.am.miyamoto@nts.ricoh.co.jp>
2015-06-29 17:24:58 +0900

    modify to use session

--- 03/src/index.js
+++ 03/src/index.js
@@ -5,8 +5,8 @@ var bodyParser = require('body-parser');
 var config = require('./config');
 var Database = require('./model/users');
 var validater = require('../public/js/validation');
-var db = new Database(config.dbhost);
-
+// var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var app = express();
 
 // setting
@@ -15,99 +15,28 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // middle ware
+// app.use(cookieParser());
+app.use(session({ secret: 'vcp training server' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
 
-// Content-type
-// application/x-www-form-urlencoded -> username=soneda&item=10
-// application/json
-// multipart/form-data (file upload)
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// routing
-// GET http://localhost:3000/hello -> world
-app.get('/hello', function(req, res) {
-  res.send('world');
+app.get('/1', function(req, res) {
+  var name = req.query.name;
+  req.session.name = name;
+  res.send(name + ' <a href="/2">2</a>');
 });
 
-// GET http://localhost:3000/search?q=node -> node
-app.get('/search', function(req, res) {
-  console.log(req.query);
-  res.send(req.query.a+req.query.b);
+app.get('/2', function(req, res) {
+  var name = req.session.name;
+  res.send(name + ' <a href="/3">3</a>');
 });
 
-// GET http://localhost:3000/users/soneda  -> soneda
-app.get('/users/:id', function(req, res) {
-  console.log(req.path);
-  console.log(req.params);
-  res.send(req.params.id);
-});
-
-// GET
-app.get('/sendfile', function(req, res) {
-  res.sendFile(__dirname + '/public/sample.html');
-});
-
-// GET
-app.get('/submit', function(req, res) {
-  console.log(req.body);
-  console.log(req.params);
-  console.log(req.path);
-  console.log(req.query);
-  res.send(req.query.username);
-});
-
-// POST
-app.post('/submit', function(req, res) {
-  res.send(req.body);
-});
-
-// GET /ejs
-app.get('/ejs', function(req, res) {
-   var id = req.query.id;
-  // res.redirect('/hello');
-  res.render('index', {id: id});
-});
-
-app.get('/', function(req, res) {
-  res.redirect('/public/index.html');
-})
-
-app.get('/test', function(req, res){
-  var username = req.query.username;
-  if(users[username] && req.query.password === users[username]) {
-    res.send('ok');
-  } else {
-    res.status(401).send('ng');
-  }
-});
-
-app.post('/test', function(req, res){
-  var username = req.body.username;
-  var password = req.body.password;
-
-  var errors = validater.validation({ username: username, password: password });
-  if(errors.length > 0) {
-    return res.send(errors.join(','));
-  }
-
-  db.connect()
-  .then(function(result) {
-    return db.login(username, password);
-  }).then(function(result) {
-    console.log(result);
-    if (result === true) {
-      return res.send('ok');
-    }
-    return res.send('ng');
-  }).catch(function(error) {
-    console.log(error);
-    return res.send('ng');
+app.get('/3', function(req, res) {
+  var name = req.session.name;
+  req.session.destroy(function(err) {
+    res.send(name);
   });
 });
 
-// console.log(req.body);
-// console.log(req.params);
-// console.log(req.path);
-// console.log(req.query);
 
 module.exports.app = app;
