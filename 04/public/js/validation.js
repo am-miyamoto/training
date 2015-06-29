16d2264e8b5ca3750d21e7c563f0cbe4f5a76e63
miyamoto <atsushi.am.miyamoto@nts.ricoh.co.jp>
2015-06-29 14:57:47 +0900

    modify error definition

--- 04/public/js/validation.js
+++ 04/public/js/validation.js
@@ -20,13 +20,13 @@ function usernameValidater(username) {
     char_rule: new RegExp('[^(a-zA-Z\-_)]')
   };
   if (!username) {
-    errors.push(setErrorMessage('username_empty'));
+    errors.push(error_messages.username_empty);
   } else {
     if (username.length < username_rule.min_length || username.length > username_rule.max_length) {
-      errors.push(setErrorMessage('username_irregal_length'));
+      errors.push(error_messages.username_irregal_length);
     }
     if (username.match(username_rule.char_rule)) {
-      errors.push(setErrorMessage('username_irregal_chara'));
+      errors.push(error_messages.username_irregal_chara);
     }
   }
   return errors;
@@ -40,48 +40,26 @@ function passwordValidater(password) {
   };
   var errors = [];
   if (!password) {
-    errors.push(setErrorMessage('password_empty'));
+    errors.push(error_messages.password_empty);
   } else {
     if (password.length < password_rule.min_length || password.length > password_rule.max_length) {
-      errors.push(setErrorMessage('password_irregal_length'));
+      errors.push(error_messages.password_irregal_length);
     }
     if (password.match(password_rule.char_rule)) {
-      errors.push(setErrorMessage('password_irregal_chara'));
+      errors.push(error_messages.password_irregal_chara);
     }
   }
   return errors;
 }
 
-function setErrorMessage(error) {
-  var message = '';
-  error_messages.forEach(function(value) {
-    if (value.key === error) {
-      message = value.message;
-    }
-  })
-  return message;
-}
-
-var error_messages = [
-  {  key: 'username_empty',
-     message: 'usernameを入力して下さい'
-  },
-  {  key: 'username_irregal_chara',
-    message: 'usernameに使える文字は英大小文字と-(ハイフン)です'
-  },
-  {  key: 'username_irregal_length',
-    message: 'usernameは4から8文字です'
-  },
-  {  key: 'password_empty',
-     message: 'Passwordを入力して下さい'
-  },
-  {  key: 'password_irregal_chara',
-    message: 'Passwordに使える文字は英大小文字と-,+,!,@,#,*,&,^,%,~です'
-  },
-  {  key: 'password_irregal_length',
-    message: 'Passwordは6から8文字です'
-  }
-];
+var error_messages = {
+  username_empty: 'usernameを入力して下さい',
+  username_irregal_chara: 'usernameに使える文字は英大小文字と-(ハイフン)です',
+  username_irregal_length: 'usernameは4から8文字です',
+  password_empty: 'Passwordを入力して下さい',
+  password_irregal_chara: 'Passwordに使える文字は英大小文字と-,+,!,@,#,*,&,^,%,~です',
+  password_irregal_length: 'Passwordは6から8文字です'
+};
 
 if(typeof module === 'object') {
   module.exports.validation = validation;
